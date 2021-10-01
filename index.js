d7c96c57956e522d60edf82485a991bf9b2f4fb7
yash91989201 <yashraj6370@gmail.com>
2021-10-01 21:41:29 +0530

    fix: deploy to heroku for testing

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from "express";
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.listen(3000, (err) => {
   console.log(`${err ? `${err}` : `Server is started at port 3000`}`);
 });
