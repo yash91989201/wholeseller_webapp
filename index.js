426a6aa86cd3f2ba8cd34bea98f6eb9de84ec8b3
yash91989201 <yashraj6370@gmail.com>
2021-10-09 18:55:42 +0530

    fix: update backend to es5

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import express from "express";
+const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
 app.listen(PORT, (err) => {
